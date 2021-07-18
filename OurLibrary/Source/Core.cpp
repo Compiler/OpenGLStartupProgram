671e28cfae94fd9e39b6c0c8a859e32b601ce095
compiler <lukerochedev@gmail.com>
2021-07-18 17:10:38 -0400

    auto updating cmakelists

---
+++ OurLibrary/Source/Core.cpp
@@ -0,0 +1,18 @@
+#include "Core.h"
+
+
+Core::Core(){
+    std::cout << "Constructor called\n";
+}
+void Core::load(){
+    std::cout << "load called\n";
+}
+void Core::update(){
+    std::cout << "update called\n";
+}
+void Core::render(){
+    std::cout << "render called\n";
+}
+void Core::unload(){
+    std::cout << "unload called\n";
+}
---
+++ OurLibrary/Source/Core.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <iostream>
+class Core{
+
+    public:
+        Core();
+        void load();
+        void update();
+        void render();
+        void unload();
+};
